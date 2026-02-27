14c790b2e58213e37cb4354ec10035eae16329b5
Yandé Guissé <yandeiguiss2@gmail.com>
2026-02-27 00:41:57 +0000

    first commit

---
+++ src/main/java/sn/isi/l3gl/api/task_api/TaskApiApplication.java
@@ -0,0 +1,13 @@
+package sn.isi.l3gl.api.task_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/isi/l3gl/api/task_api/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.isi.l3gl.api.task_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
